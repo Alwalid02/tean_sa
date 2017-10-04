e2563ba64ec3129fb284d291695e89dfd908b455
Walid Alzubaidi <walidalzubaidi@Walids-MacBook-Pro.local>
2017-10-04 13:33:31 +0300

    Web_browser

---
+++ app/src/main/java/com/example/walidalzubaidi/web_browser/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.walidalzubaidi.web_browser;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.myWebView);
+
+        myWebView.loadUrl("https://tean.sa");
+
+
+    }
+
+}
